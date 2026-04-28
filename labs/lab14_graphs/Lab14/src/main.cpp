873dab74b31b13655f3e6c5ad18cb8b93790cdea
traiTorsan <125727933+WarFly-07@users.noreply.github.com>
2026-04-28 23:51:39 +0600

    Perviy test

--- labs/lab14_graphs/Lab14/src/main.cpp
+++ labs/lab14_graphs/Lab14/src/main.cpp
@@ -1,13 +1,95 @@
 #include <iostream>
+#include <fstream>
 
-// Lab 14
-// TODO: реализуйте решение по заданию в labs/lab14_graphs/README.md
-//
-// Рекомендация по выводу:
-// - без лишнего текста
-// - числа через пробел
-// - если несколько строк — в фиксированном порядке
+using namespace std;
+
 int main() {
-    // TODO
+    setlocale(LC_ALL, "Russian");
+
+    ifstream fin("input.txt");
+    ofstream matrixFile("matrix.txt");
+    ofstream listFile("list.txt");
+    ofstream edgesFile("edges.txt");
+
+    if (!fin.is_open()) {
+        cout << "Не удалось открыть input.txt\n";
+        return 1;
+    }
+
+    int n, m;
+    fin >> n >> m;
+
+
+    int** matrix = new int*[n];
+
+    for (int i = 0; i < n; i++) {
+        matrix[i] = new int[n];
+    }
+
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            matrix[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        fin >> a >> b;
+
+        matrix[a - 1][b - 1] = 1;
+        matrix[b - 1][a - 1] = 1;
+    }
+
+  
+    matrixFile << "Матрица смежности:\n\n";
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            matrixFile << matrix[i][j] << " ";
+        }
+        matrixFile << "\n";
+    }
+
+
+    listFile << "Список смежности:\n\n";
+
+    for (int i = 0; i < n; i++) {
+        listFile << i + 1 << ": ";
+
+        for (int j = 0; j < n; j++) {
+            if (matrix[i][j] == 1) {
+                listFile << j + 1 << " ";
+            }
+        }
+
+        listFile << "\n";
+    }
+
+
+    edgesFile << "Последовательность рёбер:\n\n";
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (matrix[i][j] == 1) {
+                edgesFile << i + 1 << " " << j + 1 << "\n";
+            }
+        }
+    }
+
+
+    for (int i = 0; i < n; i++) {
+        delete[] matrix[i];
+    }
+
+    delete[] matrix;
+
+    fin.close();
+    matrixFile.close();
+    listFile.close();
+    edgesFile.close();
+
+    cout << "Готово. Файлы matrix.txt, list.txt и edges.txt созданы.\n";
+
     return 0;
 }
